0ddff7381cadcf6d187562e708a68fca8b71f582
CaballoBayo <theufogive@gmail.com>
2024-07-02 19:41:03 -0400

    carrito 2: el regreso

--- cesar/views.py
+++ cesar/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
+from cesar.carrito import Carrito
+from cesar.models import Producto
+
 # Create your views here.
 
 def index(request):
@@ -11,5 +14,28 @@ def registro(request):
     return render(request, 'cesar/registro.html', context)
 
 def tienda(request):
-    context={}
-    return render(request, "cesar/tienda.html", context)
+    productos = Producto.objects.all()
+    return render(request, "cesar/tienda.html",{'productos':productos})
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Producto.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("Tienda")
+
+def eliminar_producto(request, producto_id):
+    Carrito =  Carrito(request)
+    producto =  Producto.objects.ge(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("Tienda")
+
+def restar_producto(request, producto_id):
+    Carrito =  Carrito(request)
+    producto =  Producto.objects.ge(id=producto_id)
+    carrito.restar(producto)
+    return redirect("Tienda")
+
+def limpiar_carrito(request):
+    carrito = carrito(request)
+    carrito.limpiar()    
+    return redirect("Carrito")
