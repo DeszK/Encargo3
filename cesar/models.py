6410c19d005333641095d59df377b35da7e4b8fc
DeszK <damnnn.0990@gmail.com>
2024-07-04 18:57:57 -0400

    login maldito

--- cesar/models.py
+++ cesar/models.py
@@ -10,11 +10,3 @@ class Producto(models.Model):
 
     def __str__(self):
         return f'{self.nombre} -> {self.precio}'
-
-class Cliente(models.Model):
-    id_cliente = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=100)
-    contrasena = models.CharField(max_length=300)
-
-    def __str__(self):
-        return self.nombre
